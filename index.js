04c0595f2d9193548e104e799fe4af5e073b7e48
hairul90 <naimhairul@gmail.com>
2025-04-02 18:46:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 export default async function handler(req, res) {
-  const r = await fetch("https://api.dexscreener.com/latest/dex/pairs", {
-    headers: {
-      "accept": "application/json",
-      "user-agent": "Mozilla/5.0"
-    }
-  });
-  const json = await r.json();
-  res.status(200).json(json);
+  try {
+    const r = await fetch("https://api.dexscreener.com/latest/dex/pairs", {
+      headers: {
+        "accept": "application/json",
+        "user-agent": "Mozilla/5.0"
+      }
+    });
+
+    const json = await r.json();
+
+    res.status(200).json(json);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
